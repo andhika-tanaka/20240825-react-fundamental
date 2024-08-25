8b8e846e60b41872745d08e6f30d778280f51858
ANDHIKA TANAKA <andhika.tanaka@gmail.com>
2024-08-25 20:37:16 +0800

    20240825 onClick and onKeydown methods

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,15 +3,19 @@ import { useState } from "react";
 function Search(props) {
     const [keyword, setKeyword] = useState("");
     
-    const onChangeSearch = (e) => {
-        setKeyword(e.target.value);
-        props.onChangeSearch(e.target.value);
+    const onChangeSearch = () => {
+        props.onChangeSearch(keyword);
+    }
+
+    const searchKeydown = (e) => {
+        e.key==="Enter" && onChangeSearch(); 
     }
 
     return (
         <>
             <div>
-                Cari artikel : <input onChange={onChangeSearch} ></input>
+                Cari artikel : <input onChange={(e) => setKeyword(e.target.value)} onKeyDown={searchKeydown} ></input>
+                <button onClick={onChangeSearch}> Search</button>
             </div>
             {props.totalPost ? <small>Found {props.totalPost} article with keyword {keyword}</small> : "No article found!"}
             
