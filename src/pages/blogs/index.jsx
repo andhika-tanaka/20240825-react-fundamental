cf3709d9d324d64cc7b54516b26c061ac57e99eb
ANDHIKA TANAKA <andhika.tanaka@gmail.com>
2024-08-26 10:43:22 +0800

    20240826 React Route data loader

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -1,15 +1,8 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 function Blog() {
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then((response) => response.json())
-            .then((json) => setPosts(json));
-    }, []);
-
+    const posts = useLoaderData();
+    
     return (
         <>
             <h2>External Post</h2>
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,6 +4,7 @@ import Home from "../pages";
 import About from "../pages/About";
 import Blog from "../pages/blogs";
 import Post from "../pages/blogs/_id"
+import { posts, postById } from "../apis/loaders";
 
 export const router = createBrowserRouter([
     {
@@ -21,10 +22,12 @@ export const router = createBrowserRouter([
             {
                 path: "/blog",
                 element: <Blog />,
+                loader: posts,
             },
             {
                 path: "/blog/:id",
                 element: <Post />,
+                loader: postById,
             },
         ]
     },
