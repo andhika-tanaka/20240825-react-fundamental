05d67f55ac074924644e9315a85571affd28e9a1
ANDHIKA TANAKA <andhika.tanaka@gmail.com>
2024-08-26 08:54:34 +0800

    20240826 fetch API using useEffect

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,25 +1,40 @@
 import postsData from '../posts.json';
 import Article from '../components/Article'
 import Search from '../components/Search'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Home() {
-    const [posts,setPosts] = useState(postsData);
+    const [posts, setPosts] = useState(postsData);
     const [totalPost, setTotalPost] = useState(0);
+    const [extPosts, setExtPosts] = useState([]);
 
     const onChangeSearch = (value) => {
-        const filteredPosts = postsData.filter((item) => item.title.toLowerCase().includes(value));
+        const filteredPosts = postsData.filter((item) =>
+            item.title.toLowerCase().includes(value)
+        );
         setPosts(filteredPosts);
         setTotalPost(filteredPosts.length);
     }
 
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((json) => setExtPosts(json));
+    }, []);
+
+
     return (
         <>
             <h1>Simple Blog</h1>
-            <Search onChangeSearch={onChangeSearch} totalPost={totalPost}/>
+            <Search onChangeSearch={onChangeSearch} totalPost={totalPost} />
             {posts.map((props, index) =>
                 <Article {...props} key={index} />
             )}
+            <hr />
+            <h2>External Post</h2>
+            {extPosts.map((item, index) => (
+                <div key={index}> - {item.title}</div>
+            ))}
         </>
     );
 }
